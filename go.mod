aa9d0a5d09f891193c75d754f093f1f09a1cb914
wjh <admin@wjhwjhn.com>
2023-07-05 23:03:13 +0800

    添加功能 1. 端口扫描功能 2. 端口信息到协议的转换 3. 基础的http头中判断服务器信息

---
+++ go.mod
@@ -0,0 +1,21 @@
+module ScanMaster
+
+go 1.19
+
+require (
+	github.com/google/cel-go v0.16.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/shadow1ng/fscan v0.0.0-20230629132632-4cc65afe1449
+	golang.org/x/net v0.11.0
+	golang.org/x/text v0.10.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20230629202037-9506855d4529
+	google.golang.org/protobuf v1.31.0
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230305170008-8188dc5388df // indirect
+	github.com/stoewer/go-strcase v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+)
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"ScanMaster/Plugins"
+	"ScanMaster/WebScan/lib"
+	"ScanMaster/common"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var Mutex = &sync.Mutex{}
+
+var PluginList = map[string]interface{}{
+	"1000003": Plugins.WebTitle,
+}
+
+func AddScan(scantype string, info common.HostInfo, ch *chan struct{}, wg *sync.WaitGroup) {
+	*ch <- struct{}{}
+	wg.Add(1)
+	go func() {
+		Mutex.Lock()
+		common.Num += 1
+		Mutex.Unlock()
+		ScanFunc(&scantype, &info)
+		Mutex.Lock()
+		common.End += 1
+		Mutex.Unlock()
+		wg.Done()
+		<-*ch
+	}()
+}
+
+func ScanFunc(name *string, info *common.HostInfo) {
+	f := reflect.ValueOf(PluginList[*name])
+	in := []reflect.Value{reflect.ValueOf(info)}
+	f.Call(in)
+}
+
+func main() {
+	lib.Inithttp(common.Pocinfo)
+
+	Host := "16.163.13.0/24"
+	Hosts, err := common.ParseIP(Host, common.HostFile, common.NoHosts)
+	if err != nil {
+		fmt.Println("len(hosts)==0", err)
+		return
+	}
+	var ch = make(chan struct{}, common.Threads)
+	var wg = sync.WaitGroup{}
+
+	//AlivePorts := Plugins.PortScan(Hosts, fmt.Sprintf("%s,%s,%s", common.DefaultPorts, common.Webport, common.Top50Ports), common.Timeout)
+	AlivePorts := Plugins.PortScan(Hosts, common.DefaultPorts, common.Timeout)
+
+	for _, addr := range AlivePorts {
+		var info common.HostInfo
+		info.Host, info.Ports = strings.Split(addr, ":")[0], strings.Split(addr, ":")[1]
+		port, _ := strconv.Atoi(info.Ports)
+		fmt.Printf("%s:%s %s\n", info.Host, info.Ports, common.ProtocolName(port))
+		if info.Ports == "80" {
+			AddScan("1000003", info, &ch, &wg)
+		}
+	}
+	wg.Wait()
+}
