f78cbffbb55be49581e4cb33e640d55926b5e5a0
wjh <admin@wjhwjhn.com>
2023-07-10 20:12:22 +0800

    规则信息结构体

---
+++ common/rules.go
@@ -0,0 +1,17 @@
+package common
+
+type RuleData struct {
+	Name string
+	Type string
+	Rule string
+}
+
+type Md5Data struct {
+	Name   string
+	Md5Str string
+}
+
+type PocData struct {
+	Name  string
+	Alias string
+}
