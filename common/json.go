6fb84086bda02b27b626355e298b4d1bbb86d570
wjh <admin@wjhwjhn.com>
2023-07-11 16:56:13 +0800

    修正 Json 输出错误 & 新增 SetServiceProtocol 函数

--- common/json.go
+++ common/json.go
@@ -66,6 +66,20 @@ func (r *ResultInfo) AddServiceApp(ipPort string, serviceApps ...string) {
 	}
 }
 
+func (r *ResultInfo) SetServiceProtocol(ipPort string, newProtocol string) {
+	ip, port, _ := net.SplitHostPort(ipPort)
+	value, _ := r.syncMap.LoadOrStore(ip, &IPInfo{})
+	ipInfo := value.(*IPInfo)
+
+	// 遍历查找 Services 中对应端口并修改 Protocol
+	for i, s := range ipInfo.Services {
+		if strconv.Itoa(s.Port) == port {
+			ipInfo.Services[i].Protocol = newProtocol
+			return
+		}
+	}
+}
+
 func (r *ResultInfo) AddHoneypot(ipPort string, honeypot string) {
 	ip, port, _ := net.SplitHostPort(ipPort)
 	value, _ := r.syncMap.LoadOrStore(ip, &IPInfo{})
@@ -74,22 +88,13 @@ func (r *ResultInfo) AddHoneypot(ipPort string, honeypot string) {
 }
 
 func (r *ResultInfo) JsonOutput() {
-	file, err := os.Create("results.json")
-	if err != nil {
-		return
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-
+	tmpMap := make(map[string]*IPInfo)
 	r.syncMap.Range(func(key, value interface{}) bool {
-		ip := key.(string)
-		ipInfo := value.(*IPInfo)
-		err := encoder.Encode(map[string]IPInfo{ip: *ipInfo})
-		if err != nil {
-			return true
-		}
-		return false
+		tmpMap[key.(string)] = value.(*IPInfo)
+		return true
 	})
+	result, err := json.MarshalIndent(tmpMap, "", "  ")
+	if err == nil {
+		os.WriteFile("results.json", result, 0666)
+	}
 }
