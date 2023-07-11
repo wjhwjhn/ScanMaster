504eb15a4f26146e3d821aaadeaef87cf9e3d728
wjh <admin@wjhwjhn.com>
2023-07-11 11:45:16 +0800

    AddServiceApp 函数修改为可变参数形式，可传入多个 service_app

--- common/json.go
+++ common/json.go
@@ -46,7 +46,7 @@ func (r *ResultInfo) AddService(ipPort string) {
 	ipInfo.Services = append(ipInfo.Services, Service{Port: port})
 }
 
-func (r *ResultInfo) AddServiceApp(ipPort string, service_app string) {
+func (r *ResultInfo) AddServiceApp(ipPort string, serviceApps ...string) {
 	ip, port, _ := net.SplitHostPort(ipPort)
 	value, _ := r.syncMap.LoadOrStore(ip, &IPInfo{})
 	ipInfo := value.(*IPInfo)
@@ -54,7 +54,7 @@ func (r *ResultInfo) AddServiceApp(ipPort string, service_app string) {
 	// 遍历查找 Services 中对应端口并添加 service_app
 	for i, s := range ipInfo.Services {
 		if strconv.Itoa(s.Port) == port {
-			ipInfo.Services[i].Service_app = append(ipInfo.Services[i].Service_app, service_app)
+			ipInfo.Services[i].Service_app = append(ipInfo.Services[i].Service_app, serviceApps...)
 			return
 		}
 	}
