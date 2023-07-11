06a319f9de5d5c08a12aa0e2c84fe58075fc9fcb
wjh <admin@wjhwjhn.com>
2023-07-11 18:53:58 +0800

    加强 https 协议检测

--- Plugins/portscan.go
+++ Plugins/portscan.go
@@ -2,8 +2,10 @@ package Plugins
 
 import (
 	"ScanMaster/common"
+	"crypto/tls"
 	"fmt"
 	"net"
+	"net/http"
 	"sort"
 	"strconv"
 	"strings"
@@ -71,12 +73,13 @@ func PortScan(hostslist []string, probePorts []int, timeout int64, results chan<
 func DeepDetectPortProtocol(addr Addr) (net.Conn, error) {
 	//redis
 	conn, err := common.WrapperTcpWithTimeout("tcp4", fmt.Sprintf("%s:%v", addr.IP, addr.Port), time.Duration(common.Timeout)*time.Second)
-	conn.SetReadDeadline(time.Now().Add(time.Duration(common.Timeout) * time.Second))
-	if err == nil {
-		_, err := conn.Write([]byte("*1\x0d\x0a$4\x0d\x0aPING\x0d\x0a"))
-		return conn, err
+	if err != nil {
+		return nil, err
 	}
-	return nil, err
+
+	conn.SetReadDeadline(time.Now().Add(time.Duration(common.Timeout) * time.Second))
+	_, err = conn.Write([]byte("*1\x0d\x0a$4\x0d\x0aPING\x0d\x0a"))
+	return conn, err
 }
 
 func DetectPortProtocol(addr Addr, conn net.Conn) (common.NetworkEndpoint, error) {
@@ -104,6 +107,7 @@ func DetectPortProtocol(addr Addr, conn net.Conn) (common.NetworkEndpoint, error
 	n, err := conn.Read(buf[:])
 	if err != nil {
 		if strings.Contains(strings.ToLower(err.Error()), strings.ToLower("EOF")) {
+
 			conn, err = DeepDetectPortProtocol(addr)
 			if err == nil {
 				n, err = conn.Read(buf[:])
@@ -113,15 +117,31 @@ func DetectPortProtocol(addr Addr, conn net.Conn) (common.NetworkEndpoint, error
 		} else {
 			return netEndPoint, err
 		}
+
 	}
 
 	if err != nil {
-		//如果是 EOF，则根据常用端口号识别
+		//如果是EOF错误，则根据常用端口号识别
 		if strings.Contains(strings.ToLower(err.Error()), strings.ToLower("EOF")) {
-			netEndPoint.Protocol = common.ProtocolName(addr.Port)
-		} else {
-			return netEndPoint, err
+			//检查 https
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+			client := &http.Client{Transport: tr}
+
+			_, err := client.Head(fmt.Sprintf("https://%s:%v", addr.IP, addr.Port))
+			if err == nil {
+				netEndPoint.Protocol = "https"
+			} else {
+				//非 https
+				netEndPoint.Protocol = common.ProtocolName(addr.Port)
+				//如果也不是常用端口，则跳过
+				if netEndPoint.Protocol == "unknown" {
+					return netEndPoint, err
+				}
+			}
 		}
+
 	} else {
 		//如果数据有回显，则根据返回内容书别
 		service_data := string(buf[:n])
